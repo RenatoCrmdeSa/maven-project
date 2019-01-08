fa141ae412e0d70e2d717f74efb19e6bf063c8ca
rcrodriguesm <rcrodriguesm@indracompany.com>
2019-01-08 12:44:32 -0200

    last

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,10 @@ public class Greeter {
   public Greeter() {
 
   }
+  
 	/**@param someone name
-	**@return greeting string
-	**/
+	*@return greeting string
+	*/
   public final String greet(final String someone) {
     return String.format("Yoooooo, %s!", someone);
   }
